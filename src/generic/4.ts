0b53e21e994e9aa4fc5ac149c12f3e2f6d2b9695
Olga-Gromova <gromova.o.77@gmail.com>
2023-11-09 21:06:50 +0100

    HW-02

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -1,14 +1,17 @@
 /*
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
+interface ComponentProps {
+  title: string;}
 
-class Component {
+
+class Component <T>{
   constructor (public props:T) {
 
   }
 }
 
-class Page extends Component {
+class Page extends Component <ComponentProps> {
   pageInfo () {
     console.log(this.props.title);
   }
